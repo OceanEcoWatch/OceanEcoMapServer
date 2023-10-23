de9867c7583c8453e4774dce3905243b0a8115df
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2023-10-23 12:54:05 +0200

    feat: add config to manage env vars

---
+++ app/config/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import URL
+
+load_dotenv()
+
+
+# Check if DB_URL exists
+if "DB_URL" in os.environ:
+    DATABASE_URL = os.environ["DB_URL"]
+else:
+    DB_USER = os.environ["DB_USER"]
+    DB_PW = os.environ["DB_PW"]
+    DB_NAME = os.environ["DB_NAME"]
+    DB_HOST = os.environ["DB_HOST"]
+    DB_PORT = os.environ["DB_PORT"]
+
+    DATABASE_URL = URL.create(
+        "postgresql",
+        username=DB_USER,
+        password=DB_PW,  # plain (unescaped) text
+        host=DB_HOST,
+        database=DB_NAME)
