fcfa5ef7b98aad008ebcfcf3db14c75c83228824
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-05-21 10:00:45 +0200

    feat: allow all origins - cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import aoi, job, prediction_request, predictions, scl
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(predictions.router)
 app.include_router(prediction_request.router)
