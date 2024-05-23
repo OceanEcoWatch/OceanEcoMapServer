27ae89539a64d221f8d48a0857eae2dc1ffdac86
marcleerink <marc.leerink@planet.com>
2024-05-23 12:30:34 +0200

    add create model post route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routes import aoi, job, prediction_request, predictions, scl
+from app.routes import aoi, job, model, prediction_request, predictions, scl
 
 app = FastAPI()
 
@@ -23,6 +23,7 @@ app.include_router(prediction_request.router)
 app.include_router(aoi.router)
 app.include_router(job.router)
 app.include_router(scl.router)
+app.include_router(model.router)
 
 
 @app.get("/health", tags=["Health Check"])
---
+++ app/routes/model.py
@@ -0,0 +1,67 @@
+import json
+
+from fastapi import APIRouter, Body, Query
+
+from app.db.connect import Session
+from app.db.models import Model
+
+router = APIRouter()
+
+
+@router.get("/model", tags=["Model"])
+def get_model(
+    model_id: str | None = Query(None, description="Model ID"),
+    model_url: str | None = Query(None, description="Model URL"),
+    version: int | None = Query(None, description="Model version"),
+):
+    session = Session()
+    try:
+        models = (
+            session.query(Model)
+            .filter(
+                Model.model_id == model_id,
+                Model.model_url == model_url,
+                Model.version == version,
+            )
+            .all()
+        )
+        json_models = [
+            {
+                "model_id": model.model_id,
+                "model_url": model.model_url,
+                "version": model.version,
+            }
+            for model in models
+        ]
+    finally:
+        session.close()
+
+    return json_models
+
+
+@router.post("/model", tags=["Model"])
+def post_model(
+    model_id: str = Body(..., description="Model ID"),
+    model_url: str = Body(..., description="Model URL"),
+    version: int = Body(..., description="Model version"),
+):
+    session = Session()
+    try:
+        model = Model(
+            model_id=model_id,
+            model_url=model_url,
+            version=version,
+        )
+        session.add(model)
+        session.commit()
+        json_model = json.dumps(
+            {
+                "model_id": model.model_id,
+                "model_url": model.model_url,
+                "version": model.version,
+            }
+        )
+    finally:
+        session.close()
+
+    return json_model
