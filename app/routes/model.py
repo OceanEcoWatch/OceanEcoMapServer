d58a692ec2e67b94a86e65feade33f2ce272cee4
Marc Leerink <marc.leerink@code.berlin>
2024-07-09 17:01:40 +0200

    edit create model with new schema

--- app/routes/model.py
+++ app/routes/model.py
@@ -1,11 +1,22 @@
-import json
+import datetime
 
-from fastapi import APIRouter, Body, Query
+from fastapi import APIRouter, Depends, Query
+from pydantic import BaseModel, Field
 
 from app.db.connect import Session
-from app.db.models import Model
+from app.db.models import Model, ModelType
+from app.utils import get_db
 
 router = APIRouter()
+
+
+class ModelCreate(BaseModel):
+    model_id: str = Field(...)
+    model_url: str = Field(...)
+    expected_image_height: int = Field(..., example=480)
+    expected_image_width: int = Field(..., example=480)
+    type: ModelType = Field(..., example=ModelType.SEGMENTATION)
+    output_dtype: str = Field(..., example="uint8")
 
 
 @router.get("/model", tags=["Model"])
@@ -13,55 +24,41 @@ async def get_model(
     model_id: str | None = Query(None, description="Model ID"),
     model_url: str | None = Query(None, description="Model URL"),
     version: int | None = Query(None, description="Model version"),
+    db: Session = Depends(get_db),
 ):
-    session = Session()
-    try:
-        models = (
-            session.query(Model)
-            .filter(
-                Model.model_id == model_id,
-                Model.model_url == model_url,
-                Model.version == version,
-            )
-            .all()
-        )
-        json_models = [
-            {
-                "model_id": model.model_id,
-                "model_url": model.model_url,
-                "version": model.version,
-            }
-            for model in models
-        ]
-    finally:
-        session.close()
+    models = db.query(Model)
+    if model_id:
+        models = models.filter(Model.model_id == model_id)
+    if model_url:
+        models = models.filter(Model.model_url == model_url)
+    if version:
+        models = models.filter(Model.version == version)
+
+    json_models = [
+        {
+            "model_id": model.model_id,
+            "model_url": model.model_url,
+            "version": model.version,
+        }
+        for model in models
+    ]
 
     return json_models
 
 
 @router.post("/model", tags=["Model"])
-async def post_model(
-    model_id: str = Body(..., description="Model ID"),
-    model_url: str = Body(..., description="Model URL"),
-    version: int = Body(..., description="Model version"),
-):
-    session = Session()
-    try:
-        model = Model(
-            model_id=model_id,
-            model_url=model_url,
-            version=version,
-        )
-        session.add(model)
-        session.commit()
-        json_model = json.dumps(
-            {
-                "model_id": model.model_id,
-                "model_url": model.model_url,
-                "version": model.version,
-            }
-        )
-    finally:
-        session.close()
-
-    return json_model
+def create_model(model: ModelCreate, db: Session = Depends(get_db)):
+    db_model = Model(
+        model_id=model.model_id,
+        model_url=model.model_url,
+        expected_image_height=model.expected_image_height,
+        expected_image_width=model.expected_image_width,
+        type=model.type,
+        output_dtype=model.output_dtype,
+        created_at=datetime.datetime.now(),
+        version=1,
+    )
+    db.add(db_model)
+    db.commit()
+    db.refresh(db_model)
+    return db_model
